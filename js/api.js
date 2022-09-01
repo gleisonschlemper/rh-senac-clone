6ec6dfac518ec8e6603d57b5d8aaa1997755c97e
gleisonschlemper <94567082+gleisonschlemper@users.noreply.github.com>
2022-09-01 16:23:06 -0300

    gloria

--- js/api.js
+++ js/api.js
@@ -68,8 +68,8 @@ function getUrlBase(port) {
     // return "https://cors-anywhere.herokuapp.com/https://apisenac2022.herokuapp.com/api.php/" + port;
 
     // API JAVA - SPRING WEB API
-    return "https://cors-anywhere.herokuapp.com/https://spring-web-api-dio.herokuapp.com/" + port;
-
+    //return "https://cors-anywhere.herokuapp.com/https://spring-web-api-dio.herokuapp.com/" + port;
+    return "https://cors-anywhere.herokuapp.com/https://https://web-api-java.herokuapp.com/" + port;
     // folha
     // https://spring-web-api-dio.herokuapp.com/folha
 }
